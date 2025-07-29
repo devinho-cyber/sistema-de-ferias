d97fbcba79ccbae64db40d314b06653c65e52271
Patrício Samuel <patricioanjosjf@gmail.com>
2025-07-29 11:05:18 -0300

    .

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import {
   getAuth,
-  createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
